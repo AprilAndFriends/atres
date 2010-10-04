f912ebdc7f167b7977602c5a7211537bef70d439
borisblizzard <borisblizzard@9f307ee7-c90c-919b-4e07-db7d4cece5b8>
2010-10-04 11:39:03 +0000

    - fixed and improved font generator

--- trunk/scripts/win32_font_generator/fontgen.cpp
+++ trunk/scripts/win32_font_generator/fontgen.cpp
@@ -1,32 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "windows.h"
-#include "stdio.h"
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-DWORD n1=0,n2=0;
+DWORD n1 = 0;
+DWORD n2 = 0;
 
 struct CHARACTER
 {
 	int CharCode;
 	byte tex;
-	int x,y,w,dw;
-	byte data[64*64];
+	int x;
+	int y;
+	int w;
+	int aw;
+	byte data[64 * 64];
 	int nData;
 };
 
 struct TGAINFOHEADER
 {
-	WORD w,h;
-	byte bpp,Flags;
+	WORD w;
+	WORD h;
+	byte bpp, Flags;
 };
 
-short maxw=512,bitmapw=64;
-int fonth=24;
-
+short maxw = 512;
+short bitmapw = 128;
+int fonth = 32;
+float scale = 1.0f;
+char name[128] = {0};
 
 byte *data;
-bool ascii=0;
-long sx=2,sy=2;
-long nCharacters=0;
+bool ascii = 0;
+long sx = 2;
+long sy = 2;
+long nCharacters = 0;
 // windows context variables
 BITMAPINFO* bi;
 HDC dc;
@@ -34,99 +43,107 @@ HBITMAP hBMP;
 HFONT hFont;
 byte* bdata;
 
-CHARACTER characters[64000];
+CHARACTER characters[65536];
 
 void add(int ch)
 {
-	DWORD src,dst;
+	DWORD src;
+	DWORD dst;
 
 	// get character info
 	ABC abc;
-	WCHAR c[2]={0};
-	int w;
-	c[0]=ch;
-	GetCharWidthW(dc,ch,ch,&w);
-	int cc=GetCharABCWidthsW(dc,ch,ch,&abc);
+	WCHAR c[2] = {0};
+	int w = 0;
+	c[0] = ch;
+	int cc = GetCharABCWidthsW(dc, ch, ch, &abc);
+	w = abc.abcA + abc.abcB;
 	// blit character onto DC
-	memset(bdata,255,bitmapw*bitmapw*3);
-	TextOutW(dc,0,0,c,1);
+	memset(bdata, 255, bitmapw * bitmapw * 3);
+	TextOutW(dc, 0, 0, c, 1);
 
 	// fill character info
-	CHARACTER* chr=&characters[nCharacters];
+	CHARACTER* chr = &characters[nCharacters];
 
-	chr->CharCode=ch;
-	chr->w=w;
-	chr->dw=abc.abcB;
-	chr->tex=0;
-	if (chr->dw < w) chr->dw=w; else w=chr->dw;
+	chr->CharCode = ch;
+	w = chr->w = abc.abcA + abc.abcB;
+	chr->aw = w + abc.abcC;
+	chr->tex = 0;
+	chr->aw < w ? chr->aw = w : w = chr->aw;
 
 	// move to next row if we've reached the end
-	if (sx+w > maxw)
+	if (sx + w > maxw)
 	{
-		sy+=4+fonth;
-		sx=2;
+		sy += 4 + fonth;
+		sx = 2;
 	}
 
-	chr->x=sx;
-	chr->y=sy;
-	chr->nData=0;
-	for (long y=sy;y<=sy+fonth;y++)
+	chr->x = sx;
+	chr->y = sy;
+	chr->nData = 0;
+	for (long y = sy; y <= sy + fonth; y++)
 	{
-		dst=y*maxw+sx;
-		src=(bitmapw-1-y+sy)*bitmapw*3;
-		for (long x=sx;x<sx+w;x++)
+		dst = y * maxw + sx;
+		src = (bitmapw - 1 - y + sy) * bitmapw * 3;
+		for (long x = sx; x < sx + w; x++)
 		{
-			data[dst++]=chr->data[chr->nData++]=255-(bdata[src]+bdata[src+1]+bdata[src+2])/3;
-			src+=3;
+			data[dst] = chr->data[chr->nData] = 255 - (bdata[src] + bdata[src + 1] + bdata[src + 2]) / 3;
+			dst++;
+			chr->nData++;
+			src += 3;
 		}
 	}
 
 	// check for similar characters
-	for (int i=0;i<nCharacters;i++)
+	for (int i = 0; i < nCharacters; i++)
 	{
 		if (characters[i].nData == chr->nData)
 		{
-			if (memcmp(characters[i].data,chr->data,chr->nData) == 0)
+			if (memcmp(characters[i].data, chr->data, chr->nData) == 0)
 			{
 				n2++;
-				if (characters[i].CharCode == 0) return;
-				printf("rejected %X, same as %X\n",ch,characters[i].CharCode);
-				// this code if you want to keep the character but point it to 
-				// an existing image
-				*chr=characters[i];
-				chr->nData=-1;
-				chr->CharCode=ch;
-				nCharacters++;
+				if (characters[i].CharCode != 0)
+				{
+					printf("rejected %X, same as %X\n", ch, characters[i].CharCode);
+					// this code if you want to keep the character but point it to 
+					// an existing image
+					*chr = characters[i];
+					chr->nData = -1;
+					chr->CharCode = ch;
+					nCharacters++;
+				}
 				return;
 			}
 		}
 	}
-	sx+=4+w;
+	sx += 4 + w;
 	nCharacters++;
 	n1++;
 }
 
 long Power2(long size)
 {
-	long i;
-	for (i=1;;i*=2)
-		if (i >= size) break;
+	long i = 1;
+	while (i < size)
+	{
+		i *= 2;
+	}
 	return i;
 }
 
-
-
 void AddAll()
 {
-	int ranges[][2]={ // unicode ranges, according to wikipedia: http://en.wikipedia.org/wiki/Unicode
-		{0x0000,0x007F}, // Basic Latin
-		{0x0080,0x00FF}, // Latin-1 Supplement 
-		{0x0100,0x01FF}, // Latin Extended A
-		{0x0218,0x021F}, // Latin Extended B
-		{0x0370,0x037F}, // Greek
-		{0x0400,0x04FF}, // Cyrillic
-		{0x1E00,0x1EFF}, // Latin Extended Additional
-		{0x20A0,0x20CF}, // Currency symbols
+	int ranges[][2] = { // unicode ranges, according to wikipedia: http://en.wikipedia.org/wiki/Unicode
+//		{0x0000, 0x001F}, // Basic Latin (non-printable)
+		{0x0020, 0x007F}, // Basic Latin
+/*
+		{0x0080, 0x00FF}, // Latin-1 Supplement
+		{0x0100, 0x01FF}, // Latin Extended A
+		{0x0218, 0x021F}, // Latin Extended B
+		{0x0370, 0x037F}, // Greek
+		{0x0400, 0x04FF}, // Cyrillic
+		{0x1E00, 0x1EFF}, // Latin Extended Additional
+		{0x20A0, 0x20CF}, // Currency symbols
+*/
 //		{0x,0x}, // 
 //		{0x,0x}, // 
 //		{0x,0x}, // 
@@ -137,93 +154,107 @@ void AddAll()
 //		{0x,0x}, // 
 //		{0x,0x}, // 
 //		{0x,0x}, // 
-		{0,0} // end marker
+		{0, 0} // end marker
 	};
-	for (int j=0;ranges[j][1] != 0;j++)
+	for (int j = 0; ranges[j][1] != 0; j++)
 	{
-		for (int i=ranges[j][0];i<=ranges[j][1];i++)
+		for (int i = ranges[j][0]; i <= ranges[j][1]; i++)
 		{
 			add(i);
-			if (i%100 == 0) printf("%ld ( source_y=%i; n1=%ld; n2=%ld )\n",i,sy,n1,n2); 
+			if (i % 100 == 0)
+			{
+				printf("%ld ( source_y=%i; n1=%ld; n2=%ld )\n", i, sy, n1, n2);
+			}
 		}
 	}
-	printf("-------------\ndone!\n\nSummary:\n\nCharacters written: %ld\nCharacters rejected %ld\n",n1,n2);
+	printf("-------------\ndone!\n\nSummary:\n\nCharacters written: %ld\nCharacters rejected %ld\n", n1, n2);
 }
 
 void Compile()
 {
 	if (sx > 1)
 	{
-		sy+=2+25;
-		sx=1;
+		sy += 2 + 25;
+		sx = 1;
 	}
 
+	int texw = maxw;
+	int texh = Power2(sy + fonth);
 
-	int texw=maxw,texh=Power2(sy);
+	char filename[128] = "";
+	sprintf(filename, "%s.tga", name);
+	FILE* f = fopen(filename, "wb");
 
-	FILE* f=fopen("normal.tga","wb");
-
-	byte Header[12]={0,0,2,0,0,0,0,0,0,0,0,0};
+	byte Header[12] = {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	TGAINFOHEADER iHeader;
 
-	iHeader.bpp=32;
-	iHeader.w=texw;
-	iHeader.h=texh;
-	iHeader.Flags=33;
+	iHeader.bpp = 32;
+	iHeader.w = texw;
+	iHeader.h = texh;
+	iHeader.Flags = 33;
 
-	fwrite(Header,1,12,f);
-	fwrite(&iHeader,1,sizeof(iHeader),f);
-	DWORD size=texw*texh;
-	DWORD rowsize=texh,iEnd=0xffffffff-rowsize+1;
-	for (DWORD i=0;i<size;i++)
+	fwrite(Header, 1, 12, f);
+	fwrite(&iHeader, 1, sizeof(iHeader), f);
+	DWORD size = texw * texh;
+	DWORD rowsize = texh, iEnd = 0xFFFFFFFF - rowsize + 1;
+	for (DWORD i = 0; i < size; i++)
 	{
-		fputc(0xFF,f); fputc(0xFF,f); fputc(0xFF,f);
-		fputc(data[i],f);
+		fputc(0xFF, f); fputc(0xFF, f); fputc(0xFF, f);
+		fputc(data[i], f);
 	}
 
 	fclose(f);
 
-	printf("done!\n\nSummary:\n\nCharacters written: %ld\nCharacters rejected %ld\n",n1,n2);
+	printf("done!\n\nSummary:\n\nCharacters written: %ld\nCharacters rejected %ld\n", n1, n2);
 }
 
 void WriteXML()
 {
-	FILE* f=fopen("normal.font","w");
-
-	fprintf(f,"# -----------------------------------\n");
-	fprintf(f,"# ATRES Font definition file\n");
-	fprintf(f,"# -----------------------------------\n");
-	fprintf(f,"Name=normal\n");
-	fprintf(f,"Resource=normal.png\n");
-	fprintf(f,"Height=%d\n",fonth);
-	fprintf(f,"Scale=1.0\n");
-	fprintf(f,"# -----------------------------------\n");
-	fprintf(f,"# Code|X|Y|Width|Height|Advance Width\n");
-	fprintf(f,"-------------------------------------\n");
-
+	char filename[128] = "";
+	sprintf(filename, "%s.font", name);
+	FILE* f = fopen(filename, "w");
+	fprintf(f, "# -----------------------------------\n");
+	fprintf(f, "# ATRES Font definition file\n");
+	fprintf(f, "# -----------------------------------\n");
+	fprintf(f, "Name=%s\n", name);
+	fprintf(f, "Texture=%s.tga\n", name);
+	fprintf(f, "Height=%d\n", fonth);
+	fprintf(f, "Scale=%g\n", scale);
+	fprintf(f, "# -----------------------------------\n");
+	fprintf(f, "# Code|X|Y|Width|Height|Advance Width\n");
+	fprintf(f, "-------------------------------------\n");
 	CHARACTER* c;
-	for (int i=0;i<nCharacters;i++)
+	for (int i = 0; i < nCharacters; i++)
 	{
-		c=&characters[i];
-		fprintf(f,"%d %d %d %d ",c->CharCode,c->x,c->y,c->w,c->dw);
-		if (c->w == c->dw) fprintf(f,"0\n");
-		else               fprintf(f,"%d\n",c->dw);
+		c = &characters[i];
+		fprintf(f, "%d %d %d %d ", c->CharCode, c->x, c->y, c->w, c->aw);
+		c->w == c->aw ? fprintf(f, "0\n") : fprintf(f, "%d\n", c->aw);
 	}
-
 	fclose(f);
 }
 
-int main()
+int main(int argc, char** argv)
 {
-	WCHAR name[]=L"Arial Unicode MS";
-	bool bold=1;
-	DWORD quality=PROOF_QUALITY;
+	if (argc != 3 && argc != 4)
+	{
+		printf("Wrong number of arguments supplied: FONTNAME FONTSIZE [SCALE]\n");
+		return 1;
+	}
+	strcpy(name, argv[1]);
+	sscanf(argv[2], "%d", &fonth);
+	if (argc == 4)
+	{
+		sscanf(argv[3], "%f", &scale);
+	}
 
-	data=(byte*) malloc(maxw*(maxw*4)*2);
-	memset(data,0,maxw*(maxw*4)*2);
+	bool bold = 1;
+	DWORD quality = PROOF_QUALITY;
 
-	bi = (BITMAPINFO*) malloc(sizeof(BITMAPINFO));
-	memset(bi,0,sizeof(BITMAPINFO));
+	data = (byte*) malloc(maxw * (maxw * 4) * 2);
+	memset(data, 0, maxw * (maxw * 4) * 2);
+
+	bi = (BITMAPINFO*)malloc(sizeof(BITMAPINFO));
+	memset(bi, 0, sizeof(BITMAPINFO));
 
 	bi->bmiHeader.biSize        = sizeof(BITMAPINFOHEADER);
 	bi->bmiHeader.biWidth       = bitmapw;
@@ -232,12 +263,15 @@ int main()
 	bi->bmiHeader.biBitCount    = 24;
 	bi->bmiHeader.biCompression = BI_RGB;
 
-	dc=CreateCompatibleDC(NULL);
+	dc = CreateCompatibleDC(NULL);
 
-	hBMP = CreateDIBSection(dc,bi,DIB_RGB_COLORS,(void**) &bdata,NULL,0);
-	hFont=CreateFontW(fonth,0,0,0,100+bold*300,0,0,0,ANSI_CHARSET,OUT_TT_PRECIS,CLIP_DEFAULT_PRECIS,quality,DEFAULT_PITCH,name);
-	SelectObject(dc,hBMP);
-	SelectObject(dc,hFont);
+	hBMP = CreateDIBSection(dc, bi, DIB_RGB_COLORS, (void**)&bdata, NULL, 0);
+
+	WCHAR fontname[128] = {0};
+	mbstowcs((wchar_t*)fontname, name, strlen(name));
+	hFont = CreateFontW(fonth, 0, 0, 0, 100 + bold * 300, 0, 0, 0, ANSI_CHARSET, OUT_TT_PRECIS, CLIP_DEFAULT_PRECIS, quality, DEFAULT_PITCH, fontname);
+	SelectObject(dc, hBMP);
+	SelectObject(dc, hFont);
 
 	AddAll();
 	Compile();
