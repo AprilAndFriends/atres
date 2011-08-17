6e9f90836f6c26ebf5b64387d4df78a2f08ef41f
Boris Mikic <boris.blizzard@gmail.com>
2011-08-17 10:58:45 +0000

    - changed usage of CharacterDefinition& to CharacterDefinition* - fixed a small bug when detecting line width

--- src/FontResource.cpp
+++ src/FontResource.cpp
@@ -62,12 +62,12 @@ namespace atres
 		unsigned int code;
 		int byteLength;
 		int i = 0;
-		CharacterDefinition& character = CharacterDefinition();
+		CharacterDefinition* character;
 		while (i < text.size())
 		{
 			code = utf8_to_uint(&str[i], &byteLength);
-			character = this->characters[code];
-			result += (result < -character.bx * this->getScale() ? (character.aw - character.bx) : character.aw) * this->getScale();
+			character = &this->characters[code];
+			result += (result < -character->bx * this->getScale() ? (character->aw - character->bx) : character->aw) * this->getScale();
 			i += byteLength;
 		}
 		return result;
@@ -80,12 +80,12 @@ namespace atres
 		float width = 0.0f;
 		int byteLength;
 		int i = 0;
-		CharacterDefinition& character = CharacterDefinition();
+		CharacterDefinition* character;
 		while (i < text.size())
 		{
 			code = utf8_to_uint(&str[i], &byteLength);
-			character = this->characters[code];
-			width += (width < -character.bx * this->getScale() ? (character.aw - character.bx) : character.aw) * this->getScale();
+			character = &this->characters[code];
+			width += (width < -character->bx * this->getScale() ? (character->aw - character->bx) : character->aw) * this->getScale();
 			if (width > maxWidth)
 			{
 				break;
