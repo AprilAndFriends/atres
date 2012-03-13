00653927e3beb283d3e3dec4197cf254f30a5418
Boris Mikic <boris.blizzard@gmail.com>
2012-03-13 11:16:05 +0000

    - april Android update

--- demos/demo_simple/android/src/DemoSimple.java
+++ demos/demo_simple/android/src/DemoSimple.java
@@ -7,4 +7,11 @@ public class DemoSimple extends net.sourceforge.april.AprilActivity
 		System.loadLibrary("demo_simple");
 	}
 	
+	@Override
+	protected void onCreate(android.os.Bundle savedInstanceState)
+	{
+		this.forceArchivePath(this.getPackageResourcePath()); // forces APK as archive file
+		super.onCreate(savedInstanceState);
+	}
+	
 }
