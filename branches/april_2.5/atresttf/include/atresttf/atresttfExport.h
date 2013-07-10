e15831a9dbdc568db24611f99a0c1279ec248c2b
borisblizzard <borisblizzard@9f307ee7-c90c-919b-4e07-db7d4cece5b8>
2013-07-10 14:02:05 +0000

    - export fix

--- branches/april_2.5/atresttf/include/atresttf/atresttfExport.h
+++ branches/april_2.5/atresttf/include/atresttf/atresttfExport.h
@@ -18,7 +18,7 @@
 	/// @brief Macro for DLL exports/imports.
 	/// @def atresttfFnExport
 	/// @brief Macro for function DLL exports/imports.
-	#ifdef _STATICLIB
+	#ifdef _LIB
 		#define atresttfExport
 		#define atresttfFnExport
 	#else
