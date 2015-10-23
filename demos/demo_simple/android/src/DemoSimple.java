530613ac6fa7ee2b4a2e4ae87e6970f3151e2c4b
Boris Mikic <boris.blizzard@gmail.com>
2015-10-23 08:37:14 +0200

    - demo update

--- demos/demo_simple/android/src/DemoSimple.java
+++ demos/demo_simple/android/src/DemoSimple.java
@@ -11,7 +11,7 @@ public class DemoSimple extends com.april.Activity
 	protected void onCreate(android.os.Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		this.forceArchivePath(com.april.NativeInterface.ApkPath); // forces APK as archive file
+		this.forceArchivePath(com.april.NativeInterface.apkPath); // forces APK as archive file
 	}
 	
 }
