c75b4258ffaba7c7fcaae0c4fe32ab456b48ff1a
Boris Mikic <boris.blizzard@gmail.com>
2016-05-21 09:34:03 +0200

    - added a comment to getTextHeight()

--- include/atres/Renderer.h
+++ include/atres/Renderer.h
@@ -99,13 +99,29 @@ namespace atres
 		float getTextWidthUnformatted(chstr fontName, chstr text);
 		float getTextAdvanceX(chstr fontName, chstr text);
 		float getTextAdvanceXUnformatted(chstr fontName, chstr text);
+		// @note If you used getTextWidth() or makeRenderLines() to get the text width, you probably want to add a very little number to that width (e.g. 0.01)
+		//       due to floating point rounding errors.
+		// @see getTextWidth()
+		// @see makeRenderLines()
 		float getTextHeight(chstr fontName, chstr text, float maxWidth);
+		// @note If you used getTextWidth() or makeRenderLines() to get the text width, you probably want to add a very little number to that width (e.g. 0.01)
+		//       due to floating point rounding errors.
+		// @see getTextWidth()
+		// @see makeRenderLines()
 		float getTextHeightUnformatted(chstr fontName, chstr text, float maxWidth);
 		float getTextWidth(chstr text);
 		float getTextWidthUnformatted(chstr text);
 		float getTextAdvanceX(chstr text);
 		float getTextAdvanceXUnformatted(chstr text);
+		// @note If you used getTextWidth() or makeRenderLines() to get the text width, you probably want to add a very little number to that width (e.g. 0.01)
+		//       due to floating point rounding errors.
+		// @see getTextWidth()
+		// @see makeRenderLines()
 		float getTextHeight(chstr text, float maxWidth);
+		// @note If you used getTextWidth() or makeRenderLines() to get the text width, you probably want to add a very little number to that width (e.g. 0.01)
+		//       due to floating point rounding errors.
+		// @see getTextWidth()
+		// @see makeRenderLines()
 		float getTextHeightUnformatted(chstr text, float maxWidth);
 
 		void clearCache();
